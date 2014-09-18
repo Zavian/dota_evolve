112477d67314362632aca1a2822f138fd340946d
Emanuele <zavian.mail@gmail.com>
2014-09-18 21:35:53 +0200

    Ordered for better viewing in sublime text

--- scripts/vscripts/datadriven/custom_spells.lua
+++ scripts/vscripts/datadriven/custom_spells.lua
@@ -12,6 +12,54 @@ function itemRessurecting(keys)
 	end
 end
 
+function LeaveTrailTest(keys)
+	local caster = keys.caster
+
+	--Considering that I don't know how to rotate the model
+	--it's better having a high frequency so the player
+	--will be able to guess the direction
+	local trailFrequency = 2.0
+	local trailIndex = 1
+	local trails = {}
+	local trailMax = 20
+
+
+	Timers:CreateTimer(function()
+			--Here I reset whatever is in the slot where I'll place my entity
+			if(trails[trailIndex]) then 
+				trails[trailIndex]:Destroy() 
+			end
+
+			local location = caster:GetAbsOrigin() 	--The location of the player in that moment
+
+			--Creating a unit which will be neutral (so will not give vision)
+			local dummy_unit = CreateUnitByName("npc_evolve_trail", location, false, nil, nil, DOTA_TEAM_NEUTRALS)
+			--print("[TRAILS] Placed trail number "..trailIndex)
+			trails[trailIndex] = dummy_unit	--Saving the entity for optimization purposes
+
+
+			trailIndex = trailIndex + 1
+			if(trailIndex == trailMax + 1) then -- +1 for code design purposes
+				trailIndex = 1
+			end
+			local modifiers = { 
+			"modifier_invulnerable", 						--The unit will be invulnerable
+			"modifier_riki_permanent_invisibility", 		--The unit will be invisile
+			"modifier_phased",								--The unit won't collide with others
+			"MODIFIER_STATE_UNSELECTABLE"					--The unit won't be selectable
+			}
+			for i=1, table.getn(modifiers) do
+				dummy_unit:AddNewModifier(dummy_unit, nil, modifiers[i], nil)
+			end
+			return trailFrequency
+		end
+	)
+end
+
+local shotSucceded = false
+local savingTime = 0.0
+local timeSpent 
+
 --HEALER
 	--HEAL SHOT
 		local toHeal = 0
@@ -69,138 +117,135 @@ end
 		  			return tick
 		  		end
 		    end
-		)
-
-		
+		)		
 --END HEALER
 
 --TRAPPER
-	local toSlow = 0
+	--SLOW SHOT
+		local toSlow = 0
 
-	function StartedSlowShot(keys)
-		savingTime = Time()
-		print(savingTime)
-	end
+		function StartedSlowShot(keys)
+			savingTime = Time()
+			print(savingTime)
+		end
 
-	function InterruptedSlowShot(keys)
-		caster = keys.caster
-		ability = keys.ability
-		local currentTime = Time()	
-		timeSpent = math.floor(currentTime - savingTime)
-		local percentage = math.floor((timeSpent*100) / keys.MaxCast)
-		toSlow = (percentage * keys.MaxSlow) / 100
-	end
+		function InterruptedSlowShot(keys)
+			caster = keys.caster
+			ability = keys.ability
+			local currentTime = Time()	
+			timeSpent = math.floor(currentTime - savingTime)
+			local percentage = math.floor((timeSpent*100) / keys.MaxCast)
+			toSlow = (percentage * keys.MaxSlow) / 100
+		end
 
-	function SuccededSlowShot(keys)
-		toSlow = keys.MaxSlow
-	end
+		function SuccededSlowShot(keys)
+			toSlow = keys.MaxSlow
+		end
 
-	function HitSlowShot(keys)
-		target  = keys.target
-		local modifier = "modifier_item_orb_of_venom_slow"
-		print(toSlow)
-		local modifierSlow = -math.floor(toSlow)
-		local modifierData = {
-			damage = 0, 
-			slow = modifierSlow, 
-			duration = 5
-		}
-		target:AddNewModifier(target, nil, modifier, modifierData)
-	end
+		function HitSlowShot(keys)
+			target  = keys.target
+			local modifier = "modifier_item_orb_of_venom_slow"
+			print(toSlow)
+			local modifierSlow = -math.floor(toSlow)
+			local modifierData = {
+				damage = 0, 
+				slow = modifierSlow, 
+				duration = 5
+			}
+			target:AddNewModifier(target, nil, modifier, modifierData)
+		end
+	--USE NET
+		local spellCooldown = 4.0
+		local netCounted = 0
+		local maxNets = 4
 
-	local spellCooldown = 4.0
-	local netCounted = 0
-	local maxNets = 4
-
-	function UseNetCasted(keys)
-		print("Casted use_net")
-		local caster = keys.caster
-		local abilityLevel = caster:FindAbilityByName("use_net"):GetLevel()
-		caster:RemoveAbility("use_net")
-
-		caster:AddAbility("use_net_counting")
-		caster:FindAbilityByName("use_net_counting"):SetLevel(abilityLevel)
-		local spellDuration = keys.SpellDuration
-		
-
-		Timers:CreateTimer({
-			  	endTime = spellDuration,
-			  	callback = function()
-				  	--This if will trigger if the caster hasn't finished the nets
-				  	if(caster:FindAbilityByName("use_net_counting")) then
-				  	  	caster:RemoveAbility("use_net_counting")
-				  	  	caster:AddAbility("use_net")
-				  	  	caster:FindAbilityByName("use_net"):SetLevel(abilityLevel)
-				  	  	caster:FindAbilityByName("use_net"):StartCooldown(keys.SpellCooldown)
-				  	end
-			  	end
-		  	})
-	end
-
-
-	function UseNetCountingCasted(keys)
-		--print("Casted use_net_counting")
-		netCounted = netCounted + 1
-		print(netCounted)
-		if(netCounted == 4) then
-			netCounted = 0
+		function UseNetCasted(keys)
+			print("Casted use_net")
 			local caster = keys.caster
-			local abilityLevel = caster:FindAbilityByName("use_net_counting"):GetLevel()
-			caster:RemoveAbility("use_net_counting")
-	  	  	caster:AddAbility("use_net")
-	  	  	caster:FindAbilityByName("use_net"):SetLevel(abilityLevel)
-	  	  	caster:FindAbilityByName("use_net"):StartCooldown(keys.SpellCooldown)
-		end
-	end
+			local abilityLevel = caster:FindAbilityByName("use_net"):GetLevel()
+			caster:RemoveAbility("use_net")
 
-	function PlaceArena(keys)
-		print("Placing arena")
-		local center = keys.target
-		local centerLocation = center:GetAbsOrigin()
-		--print("X: "..centerLocation.x)
-		--print("Y: "..centerLocation.y)
-		
-		local boundLocation = centerLocation
-		local cog = {}
-		local cogDuration = 5
-		local cogNumber = 30
+			caster:AddAbility("use_net_counting")
+			caster:FindAbilityByName("use_net_counting"):SetLevel(abilityLevel)
+			local spellDuration = keys.SpellDuration
+			
 
-		local modifiers = { 
-			"modifier_invulnerable", 						--The unit will be invulnerable
-			"MODIFIER_STATE_UNSELECTABLE"					--The unit won't be selectable
-		}	
-		local x = centerLocation.x
-		local y = centerLocation.y
-		local r = 400
-
-		--Here I create a circle with these properties:
-		--x and y are the center location of the circle
-		--r it's the radius of the circle
-		--cogNumber is how many cogs I want in the circle
-		--cogDuration is how much will last the cog (see the summon_arena in npc_abilities)
-		--cog it's just the array which will contain all the cogs that I'm going to create	
-		for i = 1, cogNumber do
-		  local angle = i * math.pi / (cogNumber/2)
-		  local ptx, pty = x + r * math.cos( angle ), y + r * math.sin( angle )
-		  boundLocation.x = ptx
-		  boundLocation.y = pty
-		  cog[i] = CreateUnitByName("npc_evolve_arena_bound", boundLocation, false, nil, nil, TEAM_RADIANT)
-		  for j=1,table.getn(modifiers) do
-		  	cog[i]:AddNewModifier(cog[i], nil, modifiers[j], nil)
-		  end
+			Timers:CreateTimer({
+				  	endTime = spellDuration,
+				  	callback = function()
+					  	--This if will trigger if the caster hasn't finished the nets
+					  	if(caster:FindAbilityByName("use_net_counting")) then
+					  	  	caster:RemoveAbility("use_net_counting")
+					  	  	caster:AddAbility("use_net")
+					  	  	caster:FindAbilityByName("use_net"):SetLevel(abilityLevel)
+					  	  	caster:FindAbilityByName("use_net"):StartCooldown(keys.SpellCooldown)
+					  	end
+				  	end
+			  	})
 		end
 
-		Timers:CreateTimer({					-- This timer determines when the arena will be destroyed
-			  	endTime = cogDuration,
-			  	callback = function()
-			  		for i=1,table.getn(cog) do
-				  		cog[i]:Destroy()
-				  	end		  	
-			  	end
-	  	})
 
+		function UseNetCountingCasted(keys)
+			--print("Casted use_net_counting")
+			netCounted = netCounted + 1
+			print(netCounted)
+			if(netCounted == 4) then
+				netCounted = 0
+				local caster = keys.caster
+				local abilityLevel = caster:FindAbilityByName("use_net_counting"):GetLevel()
+				caster:RemoveAbility("use_net_counting")
+		  	  	caster:AddAbility("use_net")
+		  	  	caster:FindAbilityByName("use_net"):SetLevel(abilityLevel)
+		  	  	caster:FindAbilityByName("use_net"):StartCooldown(keys.SpellCooldown)
+			end
+		end
+	--ARENA
+		function PlaceArena(keys)
+			print("Placing arena")
+			local center = keys.target
+			local centerLocation = center:GetAbsOrigin()
+			--print("X: "..centerLocation.x)
+			--print("Y: "..centerLocation.y)
+			
+			local boundLocation = centerLocation
+			local cog = {}
+			local cogDuration = 5
+			local cogNumber = 30
 
-	end
+			local modifiers = { 
+				"modifier_invulnerable", 						--The unit will be invulnerable
+				"MODIFIER_STATE_UNSELECTABLE"					--The unit won't be selectable
+			}	
+			local x = centerLocation.x
+			local y = centerLocation.y
+			local r = 400
+
+			--Here I create a circle with these properties:
+			--x and y are the center location of the circle
+			--r it's the radius of the circle
+			--cogNumber is how many cogs I want in the circle
+			--cogDuration is how much will last the cog (see the summon_arena in npc_abilities)
+			--cog it's just the array which will contain all the cogs that I'm going to create	
+			for i = 1, cogNumber do
+			  local angle = i * math.pi / (cogNumber/2)
+			  local ptx, pty = x + r * math.cos( angle ), y + r * math.sin( angle )
+			  boundLocation.x = ptx
+			  boundLocation.y = pty
+			  cog[i] = CreateUnitByName("npc_evolve_arena_bound", boundLocation, false, nil, nil, TEAM_RADIANT)
+			  for j=1,table.getn(modifiers) do
+			  	cog[i]:AddNewModifier(cog[i], nil, modifiers[j], nil)
+			  end
+			end
+
+			Timers:CreateTimer({					-- This timer determines when the arena will be destroyed
+				  	endTime = cogDuration,
+				  	callback = function()
+				  		for i=1,table.getn(cog) do
+					  		cog[i]:Destroy()
+					  	end		  	
+				  	end
+		  	})
+		end
 --END TRAPPER
 
 --TRACKER
@@ -243,93 +288,33 @@ end
 		end
 --END TRACKER
 
-
-
-function LeaveTrailTest(keys)
-	local caster = keys.caster
-
-	--Considering that I don't know how to rotate the model
-	--it's better having a high frequency so the player
-	--will be able to guess the direction
-	local trailFrequency = 2.0
-	local trailIndex = 1
-	local trails = {}
-	local trailMax = 20
-
-
-	Timers:CreateTimer(function()
-			--Here I reset whatever is in the slot where I'll place my entity
-			if(trails[trailIndex]) then 
-				trails[trailIndex]:Destroy() 
-			end
-
-			local location = caster:GetAbsOrigin() 	--The location of the player in that moment
-
-			--Creating a unit which will be neutral (so will not give vision)
-			local dummy_unit = CreateUnitByName("npc_evolve_trail", location, false, nil, nil, DOTA_TEAM_NEUTRALS)
-			--print("[TRAILS] Placed trail number "..trailIndex)
-			trails[trailIndex] = dummy_unit	--Saving the entity for optimization purposes
-
-
-			trailIndex = trailIndex + 1
-			if(trailIndex == trailMax + 1) then -- +1 for code design purposes
-				trailIndex = 1
-			end
-			local modifiers = { 
-			"modifier_invulnerable", 						--The unit will be invulnerable
-			"modifier_riki_permanent_invisibility", 		--The unit will be invisile
-			"modifier_phased",								--The unit won't collide with others
-			"MODIFIER_STATE_UNSELECTABLE"					--The unit won't be selectable
+--BRAWLER	
+	--DEVASTATING FIST
+		function DevastatingFistCasted(keys)
+			local caster = keys.caster
+			local target = keys.target
+			local level = caster:FindAbilityByName("devastating_fist"):GetLevel()
+			local percentageDamage = keys.Percentage
+			local enemyHP = target:GetHealth()
+			local damage = math.floor((percentageDamage * enemyHP) / 100)
+				local damageTable = {
+				victim = target,
+				attacker = keys.caster,
+				damage = damage,
+				damage_type = DAMAGE_TYPE_PURE
 			}
-			for i=1, table.getn(modifiers) do
-				dummy_unit:AddNewModifier(dummy_unit, nil, modifiers[i], nil)
-			end
-			return trailFrequency
+			ApplyDamage(damageTable)
 		end
-	)
-end
+--END BRAWLER
 
 
 
 
-local shotSucceded = false
-local savingTime = 0.0
-local timeSpent 
-
---HealShot Zone
--------------------------------------------------------------------------------------------------------------
-
-
---End HealShot
--------------------------------------------------------------------------------------------------------------
-
---SlowShot Zone
--------------------------------------------------------------------------------------------------------------
-
-
---End PowerShot
--------------------------------------------------------------------------------------------------------------
-
--- +x -> Move Right
--- -x -> Move Left
 
 
 
 
-function DevastatingFistCasted(keys)
-	local caster = keys.caster
-	local target = keys.target
-	local level = caster:FindAbilityByName("devastating_fist"):GetLevel()
-	local percentageDamage = keys.Percentage
-	local enemyHP = target:GetHealth()
-	local damage = math.floor((percentageDamage * enemyHP) / 100)
-		local damageTable = {
-		victim = target,
-		attacker = keys.caster,
-		damage = damage,
-		damage_type = DAMAGE_TYPE_PURE
-	}
-	ApplyDamage(damageTable)
-end
+
+
 
 
