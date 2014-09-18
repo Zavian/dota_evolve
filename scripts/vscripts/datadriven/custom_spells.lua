2fccbc8f9b5e1100b7757f8b3036e5849efaa4a7
Emanuele <zavian.mail@gmail.com>
2014-09-18 21:34:15 +0200

    Ordered for better viewing in sublime text

--- scripts/vscripts/datadriven/custom_spells.lua
+++ scripts/vscripts/datadriven/custom_spells.lua
@@ -12,37 +12,238 @@ function itemRessurecting(keys)
 	end
 end
 
-function ChannelingShield(keys)
-	-- Event fired when I channel the shield
-	local caster = keys.caster
-	local target = keys.target
-	local spell = caster:FindAbilityByName("shield_target")
-	local modifier = "modifier_abaddon_aphotic_shield"
-	local n = 0
-	local duration = 3
-	local tick = 1.0
-	
-	-- A timer running every second that starts immediately on the next frame, respects pauses
-  	Timers:CreateTimer(function()
-			modifierData = {			--This will decide the modifier's behavior
-				duration = 1.0,			--The duration of the shield
-				damage_absorb = 1000,	--How much will absorb
-				radius = 0				--The explosion radius
-			}
-			target:AddNewModifier(target, nil, modifier, modifierData)
+--HEALER
+	--HEAL SHOT
+		local toHeal = 0
+		local HealShot_maxCast = 15
 
-			-- n will count how much times this timer will be fired
-			-- duration will be the casting duration
-	 		n = n + 1
-	      	if(n == duration) then 
-	      		return nil      		
-	  		else -- This will make the timer restart after tick seconds
-	  			return tick
-	  		end
-	    end
-	)
+		function StartedHealShot(keys)
+			savingTime = Time()
+			print(savingTime)
+		end
 
-end
+		function InterruptedHealShot(keys)
+			caster = keys.caster
+			ability = keys.ability
+			maxHeal = keys.MaxHeal
+			local currentTime = Time()	
+			timeSpent = math.floor(currentTime - savingTime)
+			local percentage = math.floor((timeSpent*100) / HealShot_maxCast)
+			toHeal = (percentage * maxHeal) / 100
+		end
+
+		function SuccededHealShot(keys)
+			toHeal = keys.MaxHeal
+		end
+
+		function HitHealShot(keys)
+			target  = keys.target	
+			target:Heal(toHeal, nil)
+		end
+	--SHIELD TARGET
+		function ChannelingShield(keys)
+		-- Event fired when I channel the shield
+		local caster = keys.caster
+		local target = keys.target
+		local spell = caster:FindAbilityByName("shield_target")
+		local modifier = "modifier_abaddon_aphotic_shield"
+		local n = 0
+		local duration = 3
+		local tick = 1.0
+		
+		-- A timer running every second that starts immediately on the next frame, respects pauses
+	  	Timers:CreateTimer(function()
+				modifierData = {			--This will decide the modifier's behavior
+					duration = 1.0,			--The duration of the shield
+					damage_absorb = 1000,	--How much will absorb
+					radius = 0				--The explosion radius
+				}
+				target:AddNewModifier(target, nil, modifier, modifierData)
+
+				-- n will count how much times this timer will be fired
+				-- duration will be the casting duration
+		 		n = n + 1
+		      	if(n == duration) then 
+		      		return nil      		
+		  		else -- This will make the timer restart after tick seconds
+		  			return tick
+		  		end
+		    end
+		)
+
+		
+--END HEALER
+
+--TRAPPER
+	local toSlow = 0
+
+	function StartedSlowShot(keys)
+		savingTime = Time()
+		print(savingTime)
+	end
+
+	function InterruptedSlowShot(keys)
+		caster = keys.caster
+		ability = keys.ability
+		local currentTime = Time()	
+		timeSpent = math.floor(currentTime - savingTime)
+		local percentage = math.floor((timeSpent*100) / keys.MaxCast)
+		toSlow = (percentage * keys.MaxSlow) / 100
+	end
+
+	function SuccededSlowShot(keys)
+		toSlow = keys.MaxSlow
+	end
+
+	function HitSlowShot(keys)
+		target  = keys.target
+		local modifier = "modifier_item_orb_of_venom_slow"
+		print(toSlow)
+		local modifierSlow = -math.floor(toSlow)
+		local modifierData = {
+			damage = 0, 
+			slow = modifierSlow, 
+			duration = 5
+		}
+		target:AddNewModifier(target, nil, modifier, modifierData)
+	end
+
+	local spellCooldown = 4.0
+	local netCounted = 0
+	local maxNets = 4
+
+	function UseNetCasted(keys)
+		print("Casted use_net")
+		local caster = keys.caster
+		local abilityLevel = caster:FindAbilityByName("use_net"):GetLevel()
+		caster:RemoveAbility("use_net")
+
+		caster:AddAbility("use_net_counting")
+		caster:FindAbilityByName("use_net_counting"):SetLevel(abilityLevel)
+		local spellDuration = keys.SpellDuration
+		
+
+		Timers:CreateTimer({
+			  	endTime = spellDuration,
+			  	callback = function()
+				  	--This if will trigger if the caster hasn't finished the nets
+				  	if(caster:FindAbilityByName("use_net_counting")) then
+				  	  	caster:RemoveAbility("use_net_counting")
+				  	  	caster:AddAbility("use_net")
+				  	  	caster:FindAbilityByName("use_net"):SetLevel(abilityLevel)
+				  	  	caster:FindAbilityByName("use_net"):StartCooldown(keys.SpellCooldown)
+				  	end
+			  	end
+		  	})
+	end
+
+
+	function UseNetCountingCasted(keys)
+		--print("Casted use_net_counting")
+		netCounted = netCounted + 1
+		print(netCounted)
+		if(netCounted == 4) then
+			netCounted = 0
+			local caster = keys.caster
+			local abilityLevel = caster:FindAbilityByName("use_net_counting"):GetLevel()
+			caster:RemoveAbility("use_net_counting")
+	  	  	caster:AddAbility("use_net")
+	  	  	caster:FindAbilityByName("use_net"):SetLevel(abilityLevel)
+	  	  	caster:FindAbilityByName("use_net"):StartCooldown(keys.SpellCooldown)
+		end
+	end
+
+	function PlaceArena(keys)
+		print("Placing arena")
+		local center = keys.target
+		local centerLocation = center:GetAbsOrigin()
+		--print("X: "..centerLocation.x)
+		--print("Y: "..centerLocation.y)
+		
+		local boundLocation = centerLocation
+		local cog = {}
+		local cogDuration = 5
+		local cogNumber = 30
+
+		local modifiers = { 
+			"modifier_invulnerable", 						--The unit will be invulnerable
+			"MODIFIER_STATE_UNSELECTABLE"					--The unit won't be selectable
+		}	
+		local x = centerLocation.x
+		local y = centerLocation.y
+		local r = 400
+
+		--Here I create a circle with these properties:
+		--x and y are the center location of the circle
+		--r it's the radius of the circle
+		--cogNumber is how many cogs I want in the circle
+		--cogDuration is how much will last the cog (see the summon_arena in npc_abilities)
+		--cog it's just the array which will contain all the cogs that I'm going to create	
+		for i = 1, cogNumber do
+		  local angle = i * math.pi / (cogNumber/2)
+		  local ptx, pty = x + r * math.cos( angle ), y + r * math.sin( angle )
+		  boundLocation.x = ptx
+		  boundLocation.y = pty
+		  cog[i] = CreateUnitByName("npc_evolve_arena_bound", boundLocation, false, nil, nil, TEAM_RADIANT)
+		  for j=1,table.getn(modifiers) do
+		  	cog[i]:AddNewModifier(cog[i], nil, modifiers[j], nil)
+		  end
+		end
+
+		Timers:CreateTimer({					-- This timer determines when the arena will be destroyed
+			  	endTime = cogDuration,
+			  	callback = function()
+			  		for i=1,table.getn(cog) do
+				  		cog[i]:Destroy()
+				  	end		  	
+			  	end
+	  	})
+
+
+	end
+--END TRAPPER
+
+--TRACKER
+	--TRACKING MINE
+		function MineLanded(keys)
+			local target = keys.target
+			spell = target:FindAbilityByName("trigger_mine")
+			if(spell) then
+				spell:SetLevel(1)
+			end
+		end
+
+		function MineTriggered(keys)
+			local caster = keys.caster
+			local casterName = caster:GetName()
+			if (string.match(casterName, "bounty")) then
+				--Gotta do the trigger mine from the hero
+			else
+				local location = caster:GetAbsOrigin()
+				local dummy_unit = CreateUnitByName("npc_evolve_vision", location, false, nil, nil, caster:GetTeam())
+				local modifiers = { 
+				"modifier_invulnerable", 						--The unit will be invulnerable
+				"modifier_riki_permanent_invisibility", 		--The unit will be invisile
+				"modifier_phased",								--The unit won't collide with others
+				"MODIFIER_STATE_UNSELECTABLE"					--The unit won't be selectable
+				}
+				for i=1, table.getn(modifiers) do
+					dummy_unit:AddNewModifier(dummy_unit, nil, modifiers[i], nil)
+				end
+				caster:Destroy()
+				
+				--Here I created a vision unit, which will give us vision for a certain time
+			  	Timers:CreateTimer({
+				  	endTime = keys.VisionDuration,
+				  	callback = function()
+				  	  dummy_unit:Destroy()
+				  	end
+			  	})
+			end
+		end
+--END TRACKER
+
+
 
 function LeaveTrailTest(keys)
 	local caster = keys.caster
@@ -88,43 +289,7 @@ function LeaveTrailTest(keys)
 	)
 end
 
-function MineLanded(keys)
-	local target = keys.target
-	spell = target:FindAbilityByName("trigger_mine")
-	if(spell) then
-		spell:SetLevel(1)
-	end
 
-end
-
-function MineTriggered(keys)
-	local caster = keys.caster
-	local casterName = caster:GetName()
-	if (string.match(casterName, "bounty")) then
-		--Gotta do the trigger mine from the hero
-	else
-		local location = caster:GetAbsOrigin()
-		local dummy_unit = CreateUnitByName("npc_evolve_vision", location, false, nil, nil, caster:GetTeam())
-		local modifiers = { 
-		"modifier_invulnerable", 						--The unit will be invulnerable
-		"modifier_riki_permanent_invisibility", 		--The unit will be invisile
-		"modifier_phased",								--The unit won't collide with others
-		"MODIFIER_STATE_UNSELECTABLE"					--The unit won't be selectable
-		}
-		for i=1, table.getn(modifiers) do
-			dummy_unit:AddNewModifier(dummy_unit, nil, modifiers[i], nil)
-		end
-		caster:Destroy()
-		
-		--Here I created a vision unit, which will give us vision for a certain time
-	  	Timers:CreateTimer({
-		  	endTime = keys.VisionDuration,
-		  	callback = function()
-		  	  dummy_unit:Destroy()
-		  	end
-	  	})
-	end
-end
 
 
 local shotSucceded = false
@@ -133,169 +298,23 @@ local timeSpent
 
 --HealShot Zone
 -------------------------------------------------------------------------------------------------------------
-local toHeal = 0
-local HealShot_maxCast = 15
 
-function StartedHealShot(keys)
-	savingTime = Time()
-	print(savingTime)
-end
-
-function InterruptedHealShot(keys)
-	caster = keys.caster
-	ability = keys.ability
-	maxHeal = keys.MaxHeal
-	local currentTime = Time()	
-	timeSpent = math.floor(currentTime - savingTime)
-	local percentage = math.floor((timeSpent*100) / HealShot_maxCast)
-	toHeal = (percentage * maxHeal) / 100
-end
-
-function SuccededHealShot(keys)
-	toHeal = keys.MaxHeal
-end
-
-function HitHealShot(keys)
-	target  = keys.target	
-	target:Heal(toHeal, nil)
-end
 
 --End HealShot
 -------------------------------------------------------------------------------------------------------------
 
 --SlowShot Zone
 -------------------------------------------------------------------------------------------------------------
-local toSlow = 0
 
-function StartedSlowShot(keys)
-	savingTime = Time()
-	print(savingTime)
-end
-
-function InterruptedSlowShot(keys)
-	caster = keys.caster
-	ability = keys.ability
-	local currentTime = Time()	
-	timeSpent = math.floor(currentTime - savingTime)
-	local percentage = math.floor((timeSpent*100) / keys.MaxCast)
-	toSlow = (percentage * keys.MaxSlow) / 100
-end
-
-function SuccededSlowShot(keys)
-	toSlow = keys.MaxSlow
-end
-
-function HitSlowShot(keys)
-	target  = keys.target
-	local modifier = "modifier_item_orb_of_venom_slow"
-	print(toSlow)
-	local modifierSlow = -math.floor(toSlow)
-	local modifierData = {
-		damage = 0, 
-		slow = modifierSlow, 
-		duration = 5
-	}
-	target:AddNewModifier(target, nil, modifier, modifierData)
-end
 
 --End PowerShot
 -------------------------------------------------------------------------------------------------------------
 
 -- +x -> Move Right
 -- -x -> Move Left
-function PlaceArena(keys)
-	print("Placing arena")
-	local center = keys.target
-	local centerLocation = center:GetAbsOrigin()
-	--print("X: "..centerLocation.x)
-	--print("Y: "..centerLocation.y)
-	
-	local boundLocation = centerLocation
-	local cog = {}
-	local cogDuration = 5
-	local cogNumber = 30
-
-	local modifiers = { 
-		"modifier_invulnerable", 						--The unit will be invulnerable
-		"MODIFIER_STATE_UNSELECTABLE"					--The unit won't be selectable
-	}	
-	local x = centerLocation.x
-	local y = centerLocation.y
-	local r = 400
-
-	--Here I create a circle with these properties:
-	--x and y are the center location of the circle
-	--r it's the radius of the circle
-	--cogNumber is how many cogs I want in the circle
-	--cogDuration is how much will last the cog (see the summon_arena in npc_abilities)
-	--cog it's just the array which will contain all the cogs that I'm going to create	
-	for i = 1, cogNumber do
-	  local angle = i * math.pi / (cogNumber/2)
-	  local ptx, pty = x + r * math.cos( angle ), y + r * math.sin( angle )
-	  boundLocation.x = ptx
-	  boundLocation.y = pty
-	  cog[i] = CreateUnitByName("npc_evolve_arena_bound", boundLocation, false, nil, nil, TEAM_RADIANT)
-	  for j=1,table.getn(modifiers) do
-	  	cog[i]:AddNewModifier(cog[i], nil, modifiers[j], nil)
-	  end
-	end
-
-	Timers:CreateTimer({					-- This timer determines when the arena will be destroyed
-		  	endTime = cogDuration,
-		  	callback = function()
-		  		for i=1,table.getn(cog) do
-			  		cog[i]:Destroy()
-			  	end		  	
-		  	end
-  	})
 
 
-end
 
-local spellCooldown = 4.0
-local netCounted = 0
-local maxNets = 4
-
-function UseNetCasted(keys)
-	print("Casted use_net")
-	local caster = keys.caster
-	local abilityLevel = caster:FindAbilityByName("use_net"):GetLevel()
-	caster:RemoveAbility("use_net")
-
-	caster:AddAbility("use_net_counting")
-	caster:FindAbilityByName("use_net_counting"):SetLevel(abilityLevel)
-	local spellDuration = 10
-	
-
-	Timers:CreateTimer({
-		  	endTime = spellDuration,
-		  	callback = function()
-			  	--This if will trigger if the caster hasn't finished the nets
-			  	if(caster:FindAbilityByName("use_net_counting")) then
-			  	  	caster:RemoveAbility("use_net_counting")
-			  	  	caster:AddAbility("use_net")
-			  	  	caster:FindAbilityByName("use_net"):SetLevel(abilityLevel)
-			  	  	caster:FindAbilityByName("use_net"):StartCooldown(spellCooldown)
-			  	end
-		  	end
-	  	})
-end
-
-
-function UseNetCountingCasted(keys)
-	--print("Casted use_net_counting")
-	netCounted = netCounted + 1
-	print(netCounted)
-	if(netCounted == 4) then
-		netCounted = 0
-		local caster = keys.caster
-		local abilityLevel = caster:FindAbilityByName("use_net_counting"):GetLevel()
-		caster:RemoveAbility("use_net_counting")
-  	  	caster:AddAbility("use_net")
-  	  	caster:FindAbilityByName("use_net"):SetLevel(abilityLevel)
-  	  	caster:FindAbilityByName("use_net"):StartCooldown(spellCooldown)
-	end
-end
 
 function DevastatingFistCasted(keys)
 	local caster = keys.caster
